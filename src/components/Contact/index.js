74350eda6985942cb1160e215f5c42edbf163654
guillaumerrrober <guillaumerrrobert@gmail.com>
2023-05-26 18:54:59 -0300

    Commit

---
+++ src/components/Contact/index.js
@@ -0,0 +1,89 @@
+import Loader from 'react-loaders'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+const Contact = () =>{
+    const reform = useRef()
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm('service_rf38e3h', 'template_b5rm8sj', reform.current, 'CnHlSWG2i9Uxd6BPS')
+            .then(
+            () => {
+                alert('Message successfully sent!');
+                window.location.reload(false);
+            },
+            () => {
+                alert('Failed to send the message, please try again');
+            }
+        );
+    };
+
+
+    return (
+        <>
+            <div className="container contact-page">
+                <div className="text-zone">
+                    <h1>
+                        <AnimatedLetters
+                            strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+                            idx={15}
+                        />
+                    </h1>
+                    <p>
+                        I am interested in freelance opportunities - especially on ambitious
+                        or large projects. However, if you have any other requests or
+                        questions, don't hesitate to contact me using below form either.
+                    </p>
+                    <div className="contact-form">
+                        <form ref={reform} onSubmit={sendEmail}>
+                            <ul>
+                                <li className="half">
+                                    <input
+                                        placeholder="Email"
+                                        type="email"
+                                        name="email"
+                                        required
+                                    />
+                                </li>
+                                <li>
+                                    <input
+                                        placeholder="Subject"
+                                        type="text"
+                                        name="subject"
+                                        required
+                                    />
+                                </li>
+                                <li>
+                                    <textarea
+                                        placeholder="Message"
+                                        name="message"
+                                        required
+                                    />
+                                </li>
+                                <li>
+                                <input type="submit" className="flat-button" value="SEND" />
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
+                </div>
+                <div className="map-wrap">
+                    <MapContainer center={[44.96366, 19.61045]} zoom={13}>
+                        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                        <Marker position={[44.96366, 19.61045]}>
+                        <Popup>Sloba lives here, come over for a cup of coffee :)</Popup>
+                        </Marker>
+                    </MapContainer>
+                </div>
+            </div>
+            <Loader type="pacman" />
+        </>
+    )
+}
+
+export default Contact
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,106 @@
+import './index.scss'
+import { Link, NavLink } from 'react-router-dom'
+import LogoS from '../../assets/images/logo-s.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faLinkedin,
+  faGithub,
+  faYoutube,
+  faSkype,
+} from '@fortawesome/free-brands-svg-icons'
+import {
+  faHome,
+  faUser,
+  faEnvelope,
+} from '@fortawesome/free-solid-svg-icons'
+// import LogoSubtitle from '../../assets/images/logo_sub.png'
+
+const Sidebar = () => {
+      return (
+    <div className="nav-bar">
+      <Link
+        className="logo"
+        to="/"
+      >
+        <img src={LogoS} alt="Logo" />
+        {/* <img className="sub-logo" src={LogoSubtitle} alt="slobodan" /> */}
+      </Link>
+      <nav>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          to="/"
+        >
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/guillaume-robert-programmer/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/guillaumerrrober"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.youtube.com/channel/UCmE4zJ78nPtVMuGVwCqD3hw"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon
+              icon={faYoutube}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="https://teams.live.com/_?utm_source=OfficeWeb#/conversations/?ctx=chat" rel="noreferrer" target="_blank">
+            <FontAwesomeIcon
+              icon={faSkype}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+      </ul>
+    </div>
+      )
+}
+
+export default Sidebar
